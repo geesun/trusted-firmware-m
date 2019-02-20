e2d37ba9f8d6555235d4fef19df70951a2c542bf
wschang0 <wschang0@nuvoton.com>
2019-02-20 16:06:52 +0800

    Add M2351 support

---
+++ cmake_m2351/b.bat
@@ -0,0 +1,2 @@
+cmake  ../ -G"Unix Makefiles" -DPROJ_CONFIG="C:/NewTFM1/trusted-firmware-m/ConfigRegression.cmake" -DTARGET_PLATFORM=M2351 -DCOMPILER=ARMCLANG -DCMAKE_BUILD_TYPE=Debug -DMBEDTLS_DEBUG=ON
+
---
+++ platform/ext/target/Nuvoton/M2351/tfm_peripherals_def.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2018, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#ifndef __TFM_PERIPHERALS_DEF_H__
+#define __TFM_PERIPHERALS_DEF_H__
+
+struct tfm_spm_partition_platform_data_t;
+
+extern struct tfm_spm_partition_platform_data_t tfm_peripheral_std_uart;
+extern struct tfm_spm_partition_platform_data_t tfm_peripheral_uart1;
+extern struct tfm_spm_partition_platform_data_t tfm_peripheral_fpga_io;
+
+#define TFM_PERIPHERAL_STD_UART  (&tfm_peripheral_std_uart)
+#define TFM_PERIPHERAL_UART1     (&tfm_peripheral_uart1)
+#define TFM_PERIPHERAL_FPGA_IO   (&tfm_peripheral_fpga_io)
+
+#endif /* __TFM_PERIPHERALS_DEF_H__ */
